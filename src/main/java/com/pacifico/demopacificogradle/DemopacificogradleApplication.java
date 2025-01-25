739c559aa646806abc6071ef98c1eca286a6d500
Jose Amadeo Diaz Diaz <jose.diaz@joedayz.pe>
2025-01-24 19:24:50 -0500

    Demo lista

--- src/main/java/com/pacifico/demopacificogradle/DemopacificogradleApplication.java
+++ src/main/java/com/pacifico/demopacificogradle/DemopacificogradleApplication.java
@@ -3,7 +3,6 @@ package com.pacifico.demopacificogradle;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
 @SpringBootApplication
 public class DemopacificogradleApplication {
 
--- src/main/java/com/pacifico/demopacificogradle/config/AzureServiceBusConfig.java
+++ src/main/java/com/pacifico/demopacificogradle/config/AzureServiceBusConfig.java
@@ -36,4 +36,5 @@ public class AzureServiceBusConfig {
         .queueName(queueName)
         .buildAsyncClient();
   }
+
 }
